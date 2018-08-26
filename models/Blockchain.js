61c54dd1b518fdbc27225a64e68ec3a7ecfe237b
Israel Calderon <calderonisrael773@gmail.com>
2018-08-26 20:59:53 +0800

    chore: Modified Blockchain model

--- models/Blockchain.js
+++ models/Blockchain.js
@@ -22,44 +22,6 @@ class Blockchain {
     this.blocks.push(block)
   }
 
-  get confirmedTransactions() {
-    return this.blocks.reduce((accumulator, block) => accumulator.concat(block.transactions))
-  }
-
-  get cumulativeDifficulty() {
-    // TO DO
-    return 0
-  }
-
-  get genesisBlock() {
-    const {
-      nullAddress, nullPubKey, nullSignature, genesisDate, faucetAddress, faucetTxVal,
-    } = config
-
-    const initialFaucetTransaction = new Transaction(nullAddress, faucetAddress, faucetTxVal, 0,
-      genesisDate, 'genesis transaction', nullPubKey, nullSignature)
-    initialFaucetTransaction.minedInBlockIndex = 0
-    initialFaucetTransaction.transferSuccessful = true
-
-    return [new Block(0, [initialFaucetTransaction], 0, undefined, undefined, nullAddress,
-      0, genesisDate, undefined)]
-  }
-
-  getTransactionByHash(transactionDataHash) {
-    const { chain } = this
-    let transactionData = null
-    const { blocks } = chain
-    let counter = 0
-
-    while (!transactionData || counter < blocks.length) {
-      const currentBlock = blocks[counter]
-      counter += 1
-      transactionData = currentBlock.transactions.find(transaction => transaction.transactionDataHash === transactionDataHash)
-    }
-
-    return transactionData
-  }
-
   addNewTransaction(transaction) {
     const {
       from,
@@ -87,15 +49,32 @@ class Blockchain {
     return newTransaction
   }
 
-  getConfirmedTransactions() {
-    return this.blocks.reduce((acc, cur) => {
-      acc.push(...cur.transactions)
-      return acc
-    }, [])
+  get confirmedTransactions() {
+    return this.blocks.reduce((accumulator, block) => accumulator.concat(block.transactions))
   }
 
-  getAllTransactions() {
-    return this.getConfirmedTransactions().concat(this.pendingTransactions)
+  createCoinbaseTransaction(toAddress) {
+    const {
+      nullAddress, coinbaseTxVal, nullPubKey, nullSignature,
+    } = config
+    const transaction = new Transaction(
+      nullAddress,
+      toAddress,
+      coinbaseTxVal,
+      0,
+      new Date().toISOString(),
+      'coinbase tx',
+      nullPubKey,
+      nullSignature,
+    )
+    transaction.minedInBlockIndex = this.getLastBlockIndex() + 1
+    transaction.transferSuccessful = true
+    return transaction
+  }
+
+  get cumulativeDifficulty() {
+    // TO DO
+    return 0
   }
 
   getAccountBalances() {
@@ -123,6 +102,86 @@ class Blockchain {
     return balances
   }
 
+  getAllTransactions() {
+    return this.getConfirmedTransactions().concat(this.pendingTransactions)
+  }
+
+  get genesisBlock() {
+    const {
+      nullAddress, nullPubKey, nullSignature, genesisDate, faucetAddress, faucetTxVal,
+    } = config
+
+    const initialFaucetTransaction = new Transaction(nullAddress, faucetAddress, faucetTxVal, 0,
+      genesisDate, 'genesis transaction', nullPubKey, nullSignature)
+    initialFaucetTransaction.minedInBlockIndex = 0
+    initialFaucetTransaction.transferSuccessful = true
+
+    return [new Block(0, [initialFaucetTransaction], 0, undefined, undefined, nullAddress,
+      0, genesisDate, undefined)]
+  }
+
+  getConfirmedTransactions() {
+    return this.blocks.reduce((acc, cur) => {
+      acc.push(...cur.transactions)
+      return acc
+    }, [])
+  }
+  
+  getTransactionByHash(transactionDataHash) {
+    const { chain } = this
+    let transactionData = null
+    const { blocks } = chain
+    let counter = 0
+
+    while (!transactionData || counter < blocks.length) {
+      const currentBlock = blocks[counter]
+      counter += 1
+      transactionData = currentBlock.transactions.find(transaction => transaction.transactionDataHash === transactionDataHash)
+    }
+
+    return transactionData
+  }
+
+  getLastBlockIndex() {
+    return this.getLastBlock().index
+  }
+
+  getLastBlock() {
+    return this.blocks.pop()
+  }
+
+  getMiningJob(address) {
+    let nextBlockIndex = this.blocks.length
+    let pendingTransactions = JSON.parse(JSON.stringify(this.getPendingTransactions()))
+      .sort((a,b) => a.fee-b.fee)
+
+    let coinbaseTransaction = this.createCoinbaseTransaction(address)
+    let balances = this.getAccountBalances()
+    for (let transaction in pendingTransactions) {
+      balances[transaction.from] = balances[transaction.from] || 0
+      balances[transaction.to] = balances[transaction.to] || 0
+      
+      if(balanceFrom >= transaction.fee) {
+        transaction.minedInBlockIndex = nextBlockIndex
+        balances[transaction.from] -= transaction.fee
+        coinbaseTransaction.value += transaction.fee
+        if(balances[transaction.from] >= transaction.value) {
+          balances[transaction.from] -= transaction.value;
+          balances[transaction.to] += transaction.value
+          transaction.transferSuccessful = true
+        } else {
+          transaction.transferSuccessful = false 
+        }
+      } else {
+        // this.
+      }
+    }
+  }
+
+  getPendingTransactions() {
+    return this.pendingTransactions
+  }
+
   getTransactionsByAddress(address) {
     // TODO: add validations
     let transactions = this.getAllTransactions()
@@ -130,33 +189,15 @@ class Blockchain {
       .sort((a, b) => a.dateCreated.localeCompare(b.dateCreated))
     return { address, transactions }
   }
-
-  getLastBlock() {
-    return this.blocks.pop()
+ 
+  removePendingTransactions(transactions) {
+    for (const t of transactions) {
+      this.pendingTransactions = this.pendingTransactions.filter((item) => {
+        return item.transactionDataHash !== t.transactionDataHash
+      })
+    }
   }
 
-  getLastBlockIndex() {
-    return this.getLastBlock().index
-  }
-
-  createCoinbaseTransaction(toAddress) {
-    const {
-      nullAddress, coinbaseTxVal, nullPubKey, nullSignature,
-    } = config
-    const transaction = new Transaction(
-      nullAddress,
-      toAddress,
-      coinbaseTxVal,
-      0,
-      new Date().toISOString(),
-      'coinbase tx',
-      nullPubKey,
-      nullSignature,
-    )
-    transaction.minedInBlockIndex = this.getLastBlockIndex() + 1
-    transaction.transferSuccessful = true
-    return transaction
-  }
 }
 
 module.exports = Blockchain

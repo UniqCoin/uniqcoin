d8a60d845e77d2000c18d8626aa3392242fac8ce
Israel Calderon <caldz@israels-air.lan>
2018-09-05 16:38:20 +0800

    feat: Added wallet app

---
+++ wallet-app/src/components/smart/Account.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Account extends Component {
+  render() {
+    return (
+      <h3>test this is Account</h3>
+    )
+  }
+}
+
+export default Account
---
+++ wallet-app/src/components/smart/Balance.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Balance extends Component {
+  render() {
+    return (
+      <h3>test this is Balance</h3>
+    )
+  }
+}
+
+export default Balance

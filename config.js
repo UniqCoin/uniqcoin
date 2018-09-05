7b9a785ca8963894610ac64726452d87870b308d
patriciarianne <ariannebelits@gmail.com>
2018-09-05 23:32:33 +0800

    Added endpoints on config

--- config.js
+++ config.js
@@ -15,6 +15,26 @@ const faucetAddress = 'notimplemented'
 const faucetTxVal = 1000000000000
 const safeConfirmCount = 6
 
+const endpoints = [
+  { method: 'GET', link: '/info', description: 'info desc' },
+  { method: 'GET', link: '/debug', description: 'debug desc' },
+  { method: 'GET', link: '/debug/reset-chain', description: 'debug reset desc' },
+  { method: 'GET', link: '/blocks', description: 'blocks desc' },
+  { method: 'GET', link: '/blocks/:index', description: 'blocks index desc' },
+  { method: 'GET', link: '/transactions/pending', description: 'transactions pending desc' },
+  { method: 'GET', link: '/transactions/confirmed', description: 'transactions confirmed desc' },
+  { method: 'GET', link: '/transactions/:hash', description: 'transactions hash desc' },
+  { method: 'GET', link: '/balances', description: 'balances desc' },
+  { method: 'GET', link: '/address/:address/transactions', description: 'address transaction desc' },
+  { method: 'GET', link: '/address/:address/balance', description: 'address balance desc' },
+  { method: 'POST', link: '/transaction/send', description: 'transaction send desc' },
+  { method: 'GET', link: '/peers', description: 'peers desc' },
+  { method: 'POST', link: '/peers/add', description: 'peers add desc' },
+  { method: 'POST', link: '/peers/notify-new-block', description: 'peers notify new block desc' },
+  { method: 'GET', link: '/mining/get-mining-job', description: 'get mining job desc' },
+  { method: 'GET', link: '/mining/submit', description: 'mining submit desc' },
+]
+
 module.exports = {
   port,
   p2pPort,
@@ -27,4 +47,5 @@ module.exports = {
   faucetAddress,
   faucetTxVal,
   safeConfirmCount,
+  endpoints,
 }

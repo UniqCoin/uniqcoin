2c469ff25313356c930ef00ef6b641ebb1a8aa3b
patriciarianne <ariannebelits@gmail.com>
2018-09-10 14:05:43 +0800

    Modified Balance component

--- wallet-app/src/components/smart/Balance.js
+++ wallet-app/src/components/smart/Balance.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import { Input, Label, Button, Container } from 'reactstrap'
 
 class Balance extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      balances: {},
+      transactions: {},
+      address: null,
+    }
+  }
+
+  async componentDidMount() {
+    let { balances, transactions } = this.state
+    
+  }
+
   render() {
     return (
-      <h3>test this is Balance</h3>
+      <Container>
+        <h3>View Account Balance</h3>
+        <Label>Address</Label>
+        <Input type="text"></Input>
+        <Button style={{ marginTop: 10}}>Display</Button>
+        <div>
+            
+        </div>
+      </Container>
     )
   }
 }

7b44bd374d5282135e656ac56f84ef91a00f2b58
patriciarianne <ariannebelits@gmail.com>
2018-09-11 15:07:48 +0800

    Modified TransactionList component

--- wallet-app/src/components/dumb/TransactionList.js
+++ wallet-app/src/components/dumb/TransactionList.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Label, Row, Col } from 'reactstrap'
+import moment from 'moment'
 
 const TransactionList = (props) => {
     const { transactions } = props
     return (
         <ListGroup>
             {
-                transactions.map(transaction => (
-                      <ListGroupItem>
-                          <ListGroupItemHeading>{transaction.value}</ListGroupItemHeading>
-                          <ListGroupItemText>
-                              {transaction.dateCreated}
-                          </ListGroupItemText>
-                      </ListGroupItem>
-                  ))  
+                transactions.length === 0 ? (
+                    <Label>No recent activity</Label>
+                ) :(
+                    transactions.map(transaction => (
+                        <ListGroupItem>
+                            <ListGroupItemHeading>{transaction.value} UC</ListGroupItemHeading>
+                            <ListGroupItemText>
+                                <Row>
+                                    <Col>
+                                      <Label>Date: {moment(transaction.dateCreated).format('MMM DD, YYYY hh:mm:ss a')}</Label>
+                                    </Col>
+                                    <Col>
+                                      <Label>to: {transaction.to}</Label>
+                                    </Col>
+                                     <Col>
+                                      <Label>from: {transaction.from}</Label>
+                                    </Col>
+                                </Row>
+                            </ListGroupItemText>
+                        </ListGroupItem>
+                    ))
+                )
             }
         </ListGroup>
     )

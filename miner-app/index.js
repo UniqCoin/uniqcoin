0df2667d170c0bdae92b774c07d8d1fb9b611fb8
Jechris Soldevilla <jech320@gmail.com>
2018-09-11 10:49:31 +0800

    Minor fix, changed mining port

--- miner-app/index.js
+++ miner-app/index.js
@@ -1,5 +1,5 @@
 const Miner = require('./models/Miner')
 const config = require('./../config')
 
-const miner = new Miner(config.serverHost, config.minerPort, 'qwerty')
+const miner = new Miner(config.serverHost, config.port, 'qwerty')
 miner.mineIndefinitely()
